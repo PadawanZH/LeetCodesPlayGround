ae4cf84374aa8f534877cb80c31e43bdbb880dd9
Andrew Zhang <zhangan19940520@gmail.com>
2018-08-01 23:42:23 +0800

    A043 wildcard matching DP solution

---
+++ LeetCodes.Test/A043WildcardMatchingTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace LeetCodes.Test
+{
+    public class A043WildcardMatchingTest : TestTemplate
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            foreach (var solution in Solutions)
+            {
+                var result = (int[]) Invoke(solution, new object[] {"zacabz", "*a?b*"});
+                ;
+            }
+        }
+    }
+}
