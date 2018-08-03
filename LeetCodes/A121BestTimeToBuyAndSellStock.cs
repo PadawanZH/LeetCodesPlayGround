9fabf0afacc315d971a52446b22001aae076e999
PadawanZH <zhangan19940520@gmail.com>
2018-08-03 20:48:38 +0800

    Add new question

---
+++ LeetCodes/A121BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCodes
+{
+    public class A121BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit_DP(int[] prices)
+        {
+            if (prices == null || prices.Length == 0)
+            {
+                return 0;
+            }
+            int[] dp = new int[prices.Length];
+
+            int minPrice = prices[0];
+            dp[0] = minPrice;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                dp[i] = Math.Max(dp[i - 1], prices[i] - minPrice);
+                minPrice = Math.Min(prices[i], minPrice);
+            }
+
+            return dp[prices.Length - 1];
+        }
+    }
+}
---
+++ LeetCodes/A746MinCostClimbingStairs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCodes
+{
+    public class A746MinCostClimbingStairs
+    {
+        public int MinCostClimbingStairs(int[] cost) {
+            int[] dp = new int[cost.Length + 1];
+
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+
+            for (int i = 2; i <= cost.Length; i++)
+            {
+                //((i < cost.Length)? cost[i] : 0) is for last step i.e. for step 10 15 30, result should be 15 because take 2 step to 15 and 2 step to reach the top
+                //hence 10 15 30 should be "10, 15, 30, 0"
+                dp[i] = Math.Min(dp[i - 2], dp[i - 1]) + ((i < cost.Length)? cost[i] : 0);
+            }
+
+            return dp[cost.Length];
+        }
+    }
+}
