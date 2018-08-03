d01b5fb342ab1cdc35cc99ef6b4d6224443425e6
Andrew Zhang <zhangan19940520@gmail.com>
2018-08-03 23:00:50 +0800

    A338 CountBits

---
+++ LeetCodes/A338CountBits.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodes
+{
+    public class A338CountBits
+    {
+        //Basic idea: if n = xxx100 then n-1 = xxx011
+        //hence, n & n-1 = xxx000
+        //therefore, bit(n) = bit(n & n-1) + 1
+        public int[] CountBits(int num) {
+            int[] res = new int[num + 1];
+            res[0] = 0;
+            
+            for (int i = 1; i <= num; i++)
+            {
+                res[i] = res[i & (i - 1)] + 1;
+            }
+
+            return res;
+        }
+    }
+}
